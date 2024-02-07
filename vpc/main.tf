abbc25ebff7df28dcc3625b3df1bd71fc640e51a
laban <labankiplagat81@gmail.com>
2024-02-07 12:43:05 +0300

    updates.

--- vpc/main.tf
+++ vpc/main.tf
@@ -53,3 +53,40 @@ resource "aws_subnet" "public-subnet-2" {
     }
   
 }
+
+resource "aws_internet_gateway" "my-internet-gateway" {
+    vpc_id = aws_vpc.demo-vpc.id
+    tags = {
+      Name = "demo-vpc-IGW"
+    }
+  
+}
+
+
+resource "aws_route_table" "public-route-table" {
+    vpc_id = aws_vpc.demo-vpc.id
+    tags = {
+      Name = "public-route-table"
+    }
+  
+}
+
+
+resource "aws_route" "public-route" {
+    route_table_id = aws_route_table.public-route-table.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-internet-gateway.id
+}
+
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+    subnet_id = aws_subnet.private-subnet-1.id
+    route_table_id = aws_route_table.public-route-table.id
+  
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+    subnet_id = aws_subnet.public-subnet-2.id
+    route_table_id = aws_route_table.public-route-table.id
+  
+}
